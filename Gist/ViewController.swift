503ea84d7d65bb094525b57d89fcdf7ed2643176
Won <trilliwon@gmail.com>
2017-03-24 06:02:10 +0900

    =

--- Gist/ViewController.swift
+++ Gist/ViewController.swift
@@ -1,25 +1,69 @@
-//
-//  ViewController.swift
-//  Gist
-//
-//  Created by Won on 09/03/2017.
-//  Copyright © 2017 Won. All rights reserved.
-//
-
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var textView: UITextView!
+	@IBOutlet weak var fetchButton: UIButton!
+
+	let viewModel = ViewModel()
+	let disposeBag = DisposeBag()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		fetchButton.rx.tap.asObservable().bindTo(viewModel.fetchButton).disposed(by: disposeBag)
+		viewModel.result.bindTo(textView.rx.text).disposed(by: disposeBag)
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
+class Gist {
+	var type: String? // type
+	var content: String? // content
+	var language: String? // language
+	var url: String? // raw_url
+	var size: Int? // size
+	var file: [String: Any]?
+
+	init(json: [String: Any]) {
+		guard let files = json["files"] as? [String: Any] else { return }
+		self.file = files
+	}
+}
+
+class ViewModel {
+
+	private struct Constants {
+		static let URLPrefix = "https://api.github.com/gists/"
+		static let URLSuffix = "96252a53a6e6f44a14f3f891e202f049"
+	}
+
+	let disposeBag = DisposeBag()
+
+	var fetchButton = PublishSubject<Void>()
+	var result = PublishSubject<String>()
+
+	var gist: Gist? {
+		didSet {
+			if let file = gist?.file {
+				DispatchQueue.main.async {
+					guard let file = file.first else { return }
+					guard let value = file.value as? [String: Any] else { return }
+					guard let content = value["content"] as? String else { return }
+					self.result.onNext(content)
+					print("didSet gist")
+				}
+			}
+		}
+	}
+
+	init() {
+		fetchButton.bindNext {
+			URLSession.shared.rx.json(url: URL(string: "\(Constants.URLPrefix)\(Constants.URLSuffix)")!).subscribe(onNext: { json in
+				guard let json = json as? [String: Any] else { return }
+				self.gist = Gist(json: json)
+			}).disposed(by: self.disposeBag)
+		}.disposed(by: disposeBag)
+	}
+}
--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -1,5 +1,127 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
 
-var str = "Hello, playground"
+/*:
+
+// MARK: of
+
+/**
+This method creates a new Observable instance with a variable number of elements.
+
+- seealso: [from operator on reactivex.io](http://reactivex.io/documentation/operators/from.html)
+
+- parameter elements: Elements to generate.
+- parameter scheduler: Scheduler to send elements on. If `nil`, elements are sent immediatelly on subscription.
+- returns: The observable sequence whose elements are pulled from the given arguments.
+*/
+
+*/
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+//let bag = DisposeBag()
+//var publishSubject = PublishSubject<String>()
+//
+//let subscription1 = publishSubject.subscribe(onNext:{
+//	print(#line, $0)
+//}).addDisposableTo(bag)
+//
+//// Subscription1 receives these 2 events, Subscription won't
+//
+//publishSubject.onNext("Hello")
+//publishSubject.onNext("Again")
+
+// Sub2 will not get "Hello" and "Again" because it susbcribed later
+//let subscription2 = publishSubject.subscribe(onNext:{
+//	print(#line,$0)
+//})
+
+//publishSubject.onNext("Both Subscriptions receive this message")
+
+let first = Observable.from([1, 2, 3, 4, 5, 6, 7])
+let second = Observable.from([10, 20, 30, 40, 50, 60, 70])
+
+// scan == reduce in swift
+
+//first.scan(0) { (seed, value) in
+//	return value + seed
+//	}.subscribe(onNext: {
+//	print($0)
+//})
+
+//let sequenceOfSequences = Observable.of(first, second)
+//
+//sequenceOfSequences
+//	.flatMap{ return $0 }
+//	.subscribe(onNext:{
+//	print($0)
+//})
+//
+
+
+//Observable.of(2,30,22,5,60,1).filter{$0 > 10}.subscribe(onNext:{
+//	print($0)
+//})
+
+//Observable.of(1,2,2, 2,11, 11, 1, 1, 3).distinctUntilChanged().subscribe(onNext:{
+//	print($0)
+//})
+//
+//Observable.of("2","3").startWith("1").subscribe(onNext:{
+//	print($0)
+//})
+
+//let publish1 = PublishSubject<Int>()
+//let publish2 = PublishSubject<Int>()
+//
+//Observable.of(publish1,publish2).merge().subscribe(onNext:{
+//	print($0)
+//})
+//publish1.onNext(20)
+//publish1.onNext(40)
+//publish1.onNext(60)
+//publish2.onNext(1)
+//publish1.onNext(80)
+//publish1.onNext(100)
+//publish2.onNext(1)
+//
+//
+//let a = Observable.of(1,2,3,4,5)
+//let b = Observable.of("a","b","c","d")
+//let c = Observable.of("a","b","c","d")
+//
+//Observable.zip(a,b,c){
+//	return ($0,$1,$2)
+// }.subscribe {
+//	print($0)
+//}
+
+print("")
+Observable.of(1,2,3,4,5).do(onNext: {
+	$0 * 10 // This has no effect on the actual subscription
+}).subscribe(onNext:{
+	print($0)
+})
+
+let publish1 = PublishSubject<Int>()
+let publish2 = PublishSubject<Int>()
+
+let concurrentScheduler = ConcurrentDispatchQueueScheduler(qos: .background)
+
+Observable.of(publish1,publish2)
+	.observeOn(concurrentScheduler)
+	.merge()
+	.subscribeOn(MainScheduler())
+	.subscribe(onNext:{
+		print($0)
+	})
+
+publish1.onNext(20)
+publish1.onNext(40)
+
+
+
+
