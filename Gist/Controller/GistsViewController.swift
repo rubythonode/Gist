6332164b915cbdf17be332a254e72996b14dbee3
Won <trilliwon@gmail.com>
2017-03-24 20:04:04 +0900

    refactored

--- Gist/Controller/GistsViewController.swift
+++ Gist/Controller/GistsViewController.swift
@@ -2,8 +2,9 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class GistsTableViewController: UITableViewController {
+class GistsViewController: UIViewController, UITableViewDelegate {
 
+	@IBOutlet weak var tableView: UITableView!
 	var viewModel = GistsViewModel()
 
 	let bag = DisposeBag()
@@ -11,43 +12,50 @@ class GistsTableViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		print(#function)
+		tableView.rx.setDelegate(self).disposed(by: bag)
 
-//		viewModel.gists.asObservable().bindTo(tableView.rx.items(cellIdentifier: "GistCell", cellType: GistCell.self)) { (row, element, cell) in
-//			cell.viewModel = GistCellViewModel(gist: element)
-//			print("\(row) : \(element) : \(cell)")
-//			}.disposed(by: bag)
+		viewModel.gists
+			.bindTo(tableView.rx.items(cellIdentifier: "GistCell", cellType: GistCell.self))
+			{ (row, element, cell) in
+				cell.viewModel = GistCellViewModel(gist: element)
+			}.disposed(by: bag)
 
-		self.navigationItem.rightBarButtonItem = self.editButtonItem
+		tableView.rx.itemSelected.asObservable().subscribe(onNext: { item in
+			self.viewModel.selectedIndexPath.value = item
+		}).disposed(by: bag)
 	}
 
-	override func numberOfSections(in tableView: UITableView) -> Int {
+	func numberOfSections(in tableView: UITableView) -> Int {
 		return viewModel.numberOfSections
 	}
 
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return viewModel.rowsPerSection[section]
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//		guard let des = segue.destination as? DetailViewController else { return }
+//		guard let indexPath = tableView.indexPathForSelectedRow else { return }
+//		des.textView.text = self.viewModel.gists[indexPath.row].content
 	}
 }
 
 class GistsViewModel {
-
 	let bag = DisposeBag()
 	var userName: String?
 	var gists: BehaviorSubject<[Gist]> = BehaviorSubject(value: [])
+	var selectedIndexPath: Variable<IndexPath> = Variable([])
 	var numberOfSections: Int = 1
 	var rowsPerSection: [Int] = [0]
 
 	init() {
-		print(#function)
 		let url = URL(string: "https://api.github.com/users/trilliwon/gists")!
 		URLSession.shared.rx.json(url: url).subscribe(onNext: { json in
-			print("gists: \(json)")
+			print("content: \(json)")
 			guard let gists = json as? [[String: Any]] else { return }
 			self.rowsPerSection[0] = gists.count
 			self.gists.onNext(gists.map({ Gist(json: $0) }))
 		}).disposed(by: bag)
-
 	}
 }
 
@@ -59,42 +67,37 @@ class GistCell: UITableViewCell {
 
 	var viewModel: GistCellViewModel? {
 		didSet {
-			DispatchQueue.main.async {
-				guard let viewModel = self.viewModel else { return }
-				viewModel.fileName
-					.bindTo(self.fileNameLabel.rx.text)
-					.disposed(by: self.bag)
-				viewModel.language
-					.bindTo(self.languageLabel.rx.text)
-					.disposed(by: self.bag)
-			}
+			self.viewModel?.fileName.bindTo(self.fileNameLabel.rx.text).disposed(by: self.bag)
+			self.viewModel?.language.bindTo(self.languageLabel.rx.text).disposed(by: self.bag)
 		}
 	}
 }
 
 class GistCellViewModel {
 
-	let disposeBag = DisposeBag()
+	let bag = DisposeBag()
 	var fileName = PublishSubject<String>()
 	var language = PublishSubject<String>()
 
-	var gist: Gist? {
-		didSet {
-			guard let gist = gist else { return }
+	init(gist: Gist) {
+
+		DispatchQueue.main.async {
+			self.fileName.onNext(gist.fileName)
+			self.language.onNext(gist.language)
+
 			URLSession.shared.rx.json(url: URL(string: "\(gist.rawUrl)")!)
 				.subscribe(onNext: { json in
 					guard let content = json as? String else { return }
 					gist.content = content
-					DispatchQueue.main.async {
-						self.fileName.onNext(gist.fileName)
-						self.language.onNext(gist.language)
-					}
-				}).disposed(by: self.disposeBag)
+				}).disposed(by: self.bag)
 		}
-	}
 
-	init(gist: Gist) {
-		self.gist = gist
+		let request = URLRequest(url: URL(string: gist.rawUrl)!)
+		URLSession.shared.rx.data(request: request)
+			.subscribe(onNext: { data in
+				guard let content = String(data: data, encoding: .utf8) else { return }
+				gist.content = content
+			}).disposed(by: self.bag)
 	}
 }
 
---
+++ Gist/DetailViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var textView: UITextView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+}
